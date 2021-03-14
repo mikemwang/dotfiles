65b8196e62861950273e727e8b049f17708fbbe4
mikemwang <mikewang91@gmail.com>
2021-03-14 15:31:19 -0400

    add fonts, remove spurrious symlinks

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/zsh
 ################################################################################
 #   nvim
 ################################################################################
 
-mkdir -p "$HOME/.config/nvim"
-mkdir -p "$HOME/.config/nvim/undo"
-ln -sf "$PWD/nvim/init.vim" "$HOME/.config/nvim"
+mkdir -p "$XDG_CONFIG_HOME/nvim"
+mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
+ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim"
 
 
 
@@ -13,16 +13,8 @@ ln -sf "$PWD/nvim/init.vim" "$HOME/.config/nvim"
 #   xresources
 ################################################################################
 
-rm -rf "$HOME/.config/X11"
-ln -s "$PWD/X11" "$HOME/.config"
-
-
-
-################################################################################
-#   xinit
-################################################################################
-
-ln -sf "$PWD/xinit/.xinitrc $HOME/.config/X11.xinitrc"
+rm -rf "$XDG_CONFIG_HOME/X11"
+ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 
 
 
@@ -30,19 +22,26 @@ ln -sf "$PWD/xinit/.xinitrc $HOME/.config/X11.xinitrc"
 #   i3
 ################################################################################
 
-rm -rf "$HOME/.config/i3"
-ln -s "$PWD/i3" "$HOME/.config"
+rm -rf "$XDG_CONFIG_HOME/i3"
+ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
 
 
 
 ################################################################################
 #   zsh
 ################################################################################
-mkdir -p "$HOME/.config/zsh"
-ln -sf "$PWD/zsh/.zshenv" "$HOME"
-ln -sf "$PWD/zsh/.zshrc" "$HOME/.config/zsh"
-ln -sf "$PWD/zsh/aliases" "$HOME/.config/zsh/aliases"
-rm -rf "$HOME/.config/zsh/external"
-ln -sf "$PWD/zsh/external" "$HOME/.config/zsh"
+mkdir -p "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
+ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
+rm -rf "$XDG_CONFIG_HOME/zsh/external"
+ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
 
 
+
+################################################################################
+#   fonts
+################################################################################
+
+mkdir -p "$XDG_DATA_HOME"
+cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
